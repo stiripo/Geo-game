2497bd8314addeed57939e0b7056142d80cf01a2
Polina_Berezhneva <stiripo13@gmail.com>
2023-03-13 12:49:24 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import { Map } from './assets/map.js';
+import spain from './assets/spain.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <div className="left-side">
+      <Map/>
     </div>
+    <div className="right-side">
+        <h1 className="big-heading">European Puzzle</h1>
+        <div className="country-name"></div>
+        <div className="country-shape">
+          <img src={spain} alt='country shape'/>
+          {/* <img src='./assets/spain.svg' alt='country shape'/> */}
+        </div>
+        <div className="score-section">
+            <p>Your score:</p>
+            <div className="score-board">
+                <div className="win">0</div>
+                <div className="lose">0</div>
+            </div>
+        </div>
+    </div>
+</div>
   );
 }
 
