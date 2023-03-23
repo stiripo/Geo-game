6f2e77e6b365b46bd742853b7bfe7c675d7018c2
Polina_Berezhneva <stiripo13@gmail.com>
2023-03-22 21:42:26 -0400

    Improve dnd to snap country into place if placed close enough

--- src/components/CurrentCountry.js
+++ src/components/CurrentCountry.js
@@ -1,45 +1,28 @@
 import { EUROPEAN_COUNTRIES, ERROR_MARGIN } from '../constants';
 import Draggable from 'react-draggable';
-import { DraggableCore } from 'react-draggable';
 import { useState } from 'react';
 
 export function CurrentCountry() {
-    // const [x, setX] = useState(0);
-    // const [y, setY] = useState(0);
-    // const [phase, setPhase] = useState('start');
 
-    // function setPosition() {
-    //     if (phase === 'start') {
-    //         return {position: 'relative'};
-    //     } else {
-    //         return {
-    //             position: 'absolute',
-    //             top: y + 'px',
-    //             left: x + 'px',
-    //             zIndex: 1000,
-    //         };
-    //     }
-    // }
+    function setPosition() {
+        return {
+            position: 'absolute',
+            top: country.top + 'px',
+            left: country.left + 'px',
+        };
+    }
+
+    const [placedCloseEnough, setPlacedCloseEnough] = useState(false);
 
     function handleDragStart(event) {
         event.preventDefault();
-        console.log("Drag start");
     }
 
     function handleDragStop(event) {
         event.preventDefault();
-        console.log(`bounding rect Y:${event.target.getBoundingClientRect().top}, X: ${event.target.getBoundingClientRect().left}`)
-        console.log(event);
-        console.log(`Drag stopped at x:${event.clientX}, y:${event.clientY}`);
         let difX = Math.abs(event.target.getBoundingClientRect().left - country.left);
-                let difY = Math.abs(event.target.getBoundingClientRect().top - country.top);
-                let placedCloseEnough = Math.sqrt(difX ** 2 + difY ** 2) < ERROR_MARGIN;
-                if (placedCloseEnough) {
-                    console.log("Close enough!");
-                } else {
-                    console.log("NOT close enough!")
-                }
-
+        let difY = Math.abs(event.target.getBoundingClientRect().top - country.top);
+        setPlacedCloseEnough(Math.sqrt(difX ** 2 + difY ** 2) < ERROR_MARGIN);
     }
 
     let index = Math.floor(Math.random() * EUROPEAN_COUNTRIES.length);
@@ -49,12 +32,18 @@ export function CurrentCountry() {
         <div className="country-shape">
             <div className="country-name">{country.name}</div>
             <Draggable
-            onStart={handleDragStart}
-            onStop={handleDragStop}
+                onStart={handleDragStart}
+                onStop={handleDragStop}
+                position={placedCloseEnough ? { x: 0, y: 0 } : undefined}
+                disabled={placedCloseEnough ? true : false}
             >
                 <img
                     src={country.image}
-                    // style={setPosition()}
+                    style={placedCloseEnough ? {
+                        position: 'absolute',
+                        top: country.top + 'px',
+                        left: country.left + 'px',
+                    } : {}}
                     alt='country shape'
                 />
             </Draggable>
