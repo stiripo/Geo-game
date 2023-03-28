41ebf2d123bed99bb9bae924ebda76f0bd2f111f
Polina_Berezhneva <stiripo13@gmail.com>
2023-03-28 11:45:30 -0400

    Render another puzzle piece after turn end
    The turn end happens after successfull placing of a puzzle piece in the correct spot, or after three unsuccessful attempts

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
 import europeMap from './assets/europe-map_clipped.svg';
+import { EUROPEAN_COUNTRIES } from './constants';
 import './App.css';
-import { CurrentCountry } from './components/CurrentCountry';
+import { PuzzlePiece } from './components/PuzzlePiece';
+import { useState } from 'react';
 
+
+function pickRandomCountry() {
+  let index = Math.floor(Math.random() * EUROPEAN_COUNTRIES.length);
+  let country = EUROPEAN_COUNTRIES[index];
+  EUROPEAN_COUNTRIES.splice(index, 1);
+  return country;
+}
+
+const initialState = pickRandomCountry();
 
 function App() {
+
+  let [puzzlePieces, setPuzzlePieces] = useState([initialState]);
+
+  function completeTurn() {
+    if (EUROPEAN_COUNTRIES.length > 0) {
+      let newPuzzlePiece = pickRandomCountry();
+      let allPuzzlePieces = [...puzzlePieces, newPuzzlePiece];
+      console.log(allPuzzlePieces)
+      setPuzzlePieces(allPuzzlePieces);
+
+    }
+}
+
   return (
     <div className="container">
-    <div className="left-side">
-      <img src={europeMap} className='europe' alt='europe map'/>
-    </div>
-    <div className="right-side">
+      <div className="left-side">
+        <img src={europeMap} className='europe' alt='europe map' />
+      </div>
+      <div className="right-side">
         <h1 className="big-heading">European Puzzle</h1>
-        
-       <CurrentCountry/>
+       {puzzlePieces.map((country) =>  <PuzzlePiece
+            country={country}
+            onTurnEnd={completeTurn}
+            />)}
+
         <div className="score-section">
-            <p>Your score:</p>
-            <div className="score-board">
-                <div className="win">0</div>
-                <div className="lose">0</div>
-            </div>
+          <p>Your score:</p>
+          <div className="score-board">
+            <div className="win">0</div>
+            <div className="lose">0</div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
   );
 }
 
---
+++ src/components/PuzzlePiece.js
@@ -0,0 +1,59 @@
+import { ERROR_MARGIN } from '../constants';
+import Draggable from 'react-draggable';
+import { useState } from 'react';
+
+export function PuzzlePiece(props) {
+
+    let [attempt, setAttempt] = useState(0);
+    let [turnEnd, setTurnEnd] = useState(false);
+
+    function handleDragStart(event) {
+        event.preventDefault();
+    }
+
+    function handleDragStop(event) {
+        event.preventDefault();
+        let difX = Math.abs(event.target.getBoundingClientRect().left - props.country.left);
+        let difY = Math.abs(event.target.getBoundingClientRect().top - props.country.top);
+        // console.log(`real coordinates: ${event.target.getBoundingClientRect().left}`);
+        // console.log(`country.left: ${props.country.left}`);
+        // console.log(difX);
+        const isCloseEnough = Math.sqrt(difX ** 2 + difY ** 2) < ERROR_MARGIN;
+        // console.log(`iscloseEnough value: ${isCloseEnough}`);
+        let currentAttempt = attempt + 1;
+        setAttempt(currentAttempt);
+        setTurnEnd(() => {
+            return isCloseEnough || currentAttempt === 3;
+        });
+        if (isCloseEnough || currentAttempt === 3) {
+            props.onTurnEnd();
+        }
+    }
+
+    return (
+        <div
+            className={turnEnd ? 'invisible' : 'country-shape'}
+        >
+            <div className="country-name">{turnEnd ? '' : props.country.name}</div>
+            <Draggable
+                onStart={handleDragStart}
+                onStop={handleDragStop}
+                position={turnEnd ? { x: 0, y: 0 } : undefined}
+                disabled={turnEnd ? true : false}
+            >
+                <img
+                    src={props.country.image}
+                    style={turnEnd ? {
+                        position: 'absolute',
+                        top: props.country.top + 'px',
+                        left: props.country.left + 'px',
+                    } : {}}
+                    alt='country shape'
+                />
+            </Draggable>
+        </div>
+    )
+}
+
+// change coordinates for checking position - not relative to the whole viewport, but relative to the map element
+// find better namespaces
