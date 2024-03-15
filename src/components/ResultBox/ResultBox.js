c2de7a10a43d576104811657bed66a75623579ef
Polina_Berezhneva <stiripo13@gmail.com>
2024-03-15 11:29:23 -0400

    Add useEffect hook for post request in ResulBox

--- src/components/ResultBox/ResultBox.js
+++ src/components/ResultBox/ResultBox.js
@@ -1,21 +1,26 @@
+import { useEffect } from 'react';
 import styles from './ResultBox.module.css';
 
 export function ResultBox(props) {
+
+  useEffect(() => {
     fetch('http://localhost:8080', {
-        method: 'POST',
-        body: JSON.stringify(props.result),
+      method: 'POST',
+      body: JSON.stringify(props.result),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        console.log(`New result ${props.result} has been sent to server`)
       })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-          console.log(`New result ${props.result} has been sent to server`)
-        })
-    return (
-        <div className={styles.result_box}>
-            Your result is:
-            <div className={styles.result_number}>{props.result}%</div>
-        </div>
-    )
+  }, []);
+
+  return (
+    <div className={styles.result_box}>
+      Your result is:
+      <div className={styles.result_number}>{props.result}%</div>
+    </div>
+  )
 
 }
